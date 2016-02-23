133528608a738f81d40e712b3d5e1462156ce5ae
donngchao <1559273720@qq.com>
2016-02-23 16:36:49 +0800

    returns the indices of the cells and columns of U,V to reconstruct the rank-1 terms

--- TKPSVD/leave2ind.m
+++ TKPSVD/leave2ind.m
@@ -3,7 +3,7 @@ function indices=leave2ind(sigmaI,n)
 % ---------------------------
 % Returns the indices of the cells and column vectors of the U,V
 % cells/vectors that are required to reconstruct the rank-1 terms
-% determined by sigmaI.
+% determined by sigmaI.返回胞元U和V的列向量的索引.
 %
 % indices   =   matrix, each row contains pairs of cell/column indices
 %
@@ -12,13 +12,6 @@ function indices=leave2ind(sigmaI,n)
 %
 % n         =   vector, dimensions of the original tensor A.
 
-% Reference
-% ---------
-%
-% A Constructive Algorithm for Decomposing a Tensor into a Finite Sum of Orthonormal Rank-1 Terms
-% http://arxiv.org/abs/1407.1593
-%
-% 2014, Kim Batselier, Haotian Liu, Ngai Wong
 
 d=length(n);                            % order of the tensor
 if d==2
@@ -37,7 +30,7 @@ for i=2:d
     endI(i)=sum(nodesperlevel(1:i));
 end
 
-%last endI points to last SVD of previous level       
+%last endI points to last SVD of previous level,这里的endI的变换至关重要       
 endI=endI(1:end-2); 
 
 % k(i) containst the offset of the ith term with respect to endI for
@@ -45,11 +38,9 @@ endI=endI(1:end-2);
 k=sigmaI;
 % l tells us which element of S{k(i)}(:,l(i)) we need to choose
 l=zeros(1,length(sigmaI));
-
 indices=zeros(length(sigmaI),2*(length(n)-1));
 
-for j=1:length(sigmaI)
-    
+for j=1:length(sigmaI)    
     % determine offset for each node
     if mod(k(j),r(end))==0
         k(j)=k(j)/r(end);
@@ -57,8 +48,7 @@ for j=1:length(sigmaI)
     else
         l(j)=mod(k(j),r(end));
         k(j)=ceil(k(j)/r(end));
-    end
-    
+    end    
     indices(j,1)=endI(end)+k(j);
     indices(j,2)=l(j);    
 end
@@ -66,8 +56,8 @@ end
 endI=endI(1:end-1);
 r=r(1:end-1);
 Tcounter=3;
-for i=1:length(endI)
-    
+
+for i=1:length(endI)%经过之前的变换，endI的长度已经变为0了，所以endI究竟想做什么呢？    
     for j=1:length(sigmaI)
         % determine offset for each node
         if mod(k(j),r(end))==0
@@ -76,26 +66,22 @@ for i=1:length(endI)
         else
             l(j)=mod(k(j),r(end));
             k(j)=ceil(k(j)/r(end));
-        end
-        
+        end        
         indices(j,Tcounter)=endI(end)+k(j);
-        indices(j,Tcounter+1)=l(j); 
-                
+        indices(j,Tcounter+1)=l(j);                 
     end    
     Tcounter=Tcounter+2;
     endI=endI(1:end-1);
     r=r(1:end-1);
 end
 
-for j=1:length(sigmaI)
-    
+for j=1:length(sigmaI)    
     % determine offset for each node
     if mod(k(j),r(end))==0
         l(j)=r(end);
     else
         l(j)=mod(k(j),r(end));
-    end
-    
+    end    
     indices(j,Tcounter)=1;
     indices(j,Tcounter+1)=l(j); 
 end
