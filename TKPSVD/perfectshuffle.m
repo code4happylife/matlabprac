e969bc40fdf508962209693d605641b3d84dbbfb
donngchao <1559273720@qq.com>
2016-02-23 09:39:20 +0800

    perfect shuffle is used to relate kron of B,C and kron of C,B.

--- TKPSVD/perfectshuffle.m
+++ TKPSVD/perfectshuffle.m
@@ -1,5 +1,5 @@
 function S = perfectshuffle(p,q)
-p =3;
+p=3;
 q=3;
 % S = perfectshuffle(p,q)
 % -------------------------
@@ -10,18 +10,14 @@ q=3;
 % p         =   scalar, number of rows of A,
 %
 % q         =   scalar, number of columns of A.
-%
-% Reference
-% ---------
-%
-% A constructive arbitrary-degree Kronecker product decomposition of matrices
-% http://arxiv.org/abs/1317261
-%
-%% 2015, Kim Batselier, Ngai Wong
+% The entries of kron(B,C) and kron(C,B)consists of all possible products
+% of a B-matrix entry with a C-matrix entry and this raises the possibility
+% that those two Kronecker products are related by a permutation.The
+% permutation involved is in fact the perfect shuffle（完全打乱，完美洗牌）.
 
 r=p*q;
-I=eye(r);
+I=eye(r);%构造一个r*r的单位矩阵
 S=[];
 for i=1:q
-   S=[S; I(i:q:r,:)];
+   S=[S; I(i:q:r,:)];%将I矩阵，按行进行洗牌操作，因为p*q=r
 end
