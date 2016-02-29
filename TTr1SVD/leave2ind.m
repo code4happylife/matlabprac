f1fe6a7516992973042ab4eb3196112c7dd20af7
donngchao <1559273720@qq.com>
2016-02-29 17:00:57 +0800

    the indices is actually used to access sigmas in different S cells in previous decompositions for restorations in later steps

--- TTr1SVD/leave2ind.m
+++ TTr1SVD/leave2ind.m
@@ -20,7 +20,7 @@ function indices=leave2ind(sigmaI,n)
 %
 % 2014, Kim Batselier, Haotian Liu, Ngai Wong
 
-d=length(n);                            % order of the tensor
+d=length(n);                            % order of the tensor，每一段程序都有这个，针对不同的阶数的张量进行分析
 if d==2
     % matrix case
     indices=ones(length(sigmaI),2);
@@ -38,20 +38,21 @@ for i=2:d
 end
 
 %last endI points to last SVD of previous level       
-endI=endI(1:end-2); 
+endI=endI(1:end-2); %endI的长度比实际的维度少2；需要遍历的树的每一层的最大的svd索引
 
 % k(i) containst the offset of the ith term with respect to endI for
 % level d-i, initalize to sigmaI
 k=sigmaI;
+% sigmaI记录的是所有非零的叶子节点所对应的索引
 % l tells us which element of S{k(i)}(:,l(i)) we need to choose
-l=zeros(1,length(sigmaI));
+l=zeros(1,length(sigmaI));%k,l和S的关系密切
 
-indices=zeros(length(sigmaI),2*(length(n)-1));
+indices=zeros(length(sigmaI),2*(length(n)-1));%较小的length(n)-1个维度进行分析
 
-for j=1:length(sigmaI)
+for j=1:length(sigmaI)%从每一个叶子节点开始分析，根据每一个叶子节点的索引开始干活
     
     % determine offset for each node
-    if mod(k(j),r(end))==0
+    if mod(k(j),r(end))==0%r(end)=2,结合它们自身的偏移量进行分析
         k(j)=k(j)/r(end);
         l(j)=r(end);
     else
@@ -101,3 +102,15 @@ for j=1:length(sigmaI)
 end
 
 end
+% [2,1,1,1;2,2,1,1;3,1,1,2;3,2,1,2;4,1,1,3;4,2,1,3]
+%计算的indices矩阵的结果是简明的
+% 只是，依然不太清楚它的具体含义，需要进一步的对svd分解和qr分解进行学习研究，并且需要对后续是具体怎样利用indices有个整体的把控
+% indices =
+% 
+%      2     1     1     1
+%      2     2     1     1
+%      3     1     1     2
+%      3     2     1     2
+%      4     1     1     3
+%      4     2     1     3
+%经过分析，初步判定，indices的各项记录的是每个奇异值在不同的S矩阵中的索引值。根据这些索引值可以更好的access这些奇异值，包括后期对于过小的奇异值的舍弃
