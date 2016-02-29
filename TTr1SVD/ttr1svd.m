10efe51bd1876a41e2c94d4b5e7403586cabc555
donngchao <1559273720@qq.com>
2016-02-29 10:42:40 +0800

    tree tree tree play with tree

--- TTr1SVD/ttr1svd.m
+++ TTr1SVD/ttr1svd.m
@@ -18,65 +18,60 @@ function [U,S,V,sigmas]=ttr1svd(A)
 %
 % sigmas    =   vector, contains the final singular values in the linear
 %               combination of rank-1 terms.
-%
-% Reference
-% ---------
-%
-% A Constructive Algorithm for Decomposing a Tensor into a Finite Sum of Orthonormal Rank-1 Terms
-% http://arxiv.org/abs/1407.1593
-%
-% 2014, Kim Batselier, Haotian Liu, Ngai Wong
 
 
+%n记录张量的维度，比如3*4*5*6*7，它是一个数组
 n=size(A);
-r=zeros(1,length(n)-1);
-for i=1:length(n)-1
-    r(i) = min(n(i),prod(n(i+1:end)));
-end
+r=zeros(1,length(n)-1);%r也是一个数组，只是它的维度会减少一个，且全部初始化为0.
+for i=1:length(n)-1%同理，根据这个数组的长度，分别对它们进行赋值。
+    r(i) = min(n(i),prod(n(i+1:end)));%赋值的时候注意，r的值分别等于n的各个维度值中，较小的length（n）-1个；r(i)的意义是，从顶点开始，每一层的节点生出的子节点的数目；
+end%r(i)都是很有意义的，分别指代了较小的n-1个维度值
 
 
-totalsvd=1;
-svdsperlevel=zeros(1,length(r));   % add length 1 for the first level
-svdsperlevel(1)=1;  % first level
+totalsvd=1;%整个需要计算的svd的数目
+svdsperlevel=zeros(1,length(r));   % add length 1 for the first level这里length(r):数组r的长度指的是树形结构的层数，即3阶张量，则r的长度为2；对应的树型数据结构，有2层树；不包括叶子节点层
+svdsperlevel(1)=1;  % first level 
 for i=2:length(r)
-    svdsperlevel(i)=prod(r(1:i-1));
-    totalsvd=totalsvd+svdsperlevel(i); 
+    svdsperlevel(i)=prod(r(1:i-1));%svdsperlevel各个元素的赋值；根据r的各项，求得svdsperlevel的个数，即，树每一层有多少个节点需要计算；对应的去迭代循环地计算svd
+    totalsvd=totalsvd+svdsperlevel(i);%totalsvd从svdsperlevel(2)开始计数，叠加 
 end
-nleaf=prod(r);%叶子节点的总数
+nleaf=prod(r);%叶子节点的总数，将r的每个元素进行乘积运算；这是树的最后一层的节点个数
 
-U=cell(1,totalsvd);
+U=cell(1,totalsvd);%胞元数据，totalsvd是所有的svd的数目
 S=cell(1,totalsvd);
 V=cell(1,totalsvd);
 
-[Ut St Vt]=svd(reshape(A,[n(1),prod(n(2:end))]),'econ');
-U{1}=Ut;
-S{1}=diag(St);
-V{1}=Vt;
+[Ut St Vt]=svd(reshape(A,[n(1),prod(n(2:end))]),'econ');%第一次计算svd，将张量进行按列进行reshape，以n(1)为行，以prod(n(2:end))为列；对这个矩阵进行svd计算
+U{1}=Ut;%将Ut的结果存储到U{1}；Ut：n(1)*n(1)
+S{1}=diag(St);%将St的对角项存储在S{1}；St：n(1)*prod(n(2:end))
+V{1}=Vt;%Vt：prod(n(2:end))*prod(n(2:end))
 counter=2; % this counter keeps track during the iterations which V{i} we are computing. This is a linear counter that counts breadth-first
 whichvcounter=1;    % this counter keeps track during the iterations of which V we are taking the svd
-sigmas=kron(S{1}, ones(nleaf/length(S{1}),1));
+sigmas=kron(S{1}, ones(nleaf/length(S{1}),1));%length(S{1})=n(1)；nleaf=prod(r)；r(i) = min(n(i),prod(n(i+1:end)))
 
-for i=1:length(r)-1           % outer loop over the levels
+for i=1:length(r)-1           % outer loop over the levels，在前面的树形结构的基础上，少遍历一层；length(r)-1=length(n)-2；3阶张量，则遍历一层；4阶张量，则遍历2层；最后一层的叶子节点不需要遍历
     Slevel=[];
-    for j=1:prod(r(1:i))      % inner loop over the number of svds for this level 
-        if rem(j,r(i)) == 0
-            col=r(i);
+    for j=1:prod(r(1:i))      % inner loop over the number of svds for this level；svdsperlevel(i)=prod(r(1:i-1))；实际这里的prod（r(1:i)）就是指svdsperlevel(2)；树每一层有多少个节点需要计算；j=[1:nodes(2)]；
+        if rem(j,r(i)) == 0   %r(i)的意义是，从顶点开始，每一层的节点生出的子节点的数目
+            col=r(i);         
         else
-            col=rem(j,r(i));
+            col=rem(j,r(i)); %偏移量的计算；
         end
         [Ut St Vt]=svd(reshape(V{whichvcounter}(:,col),[n(i+1),prod(n(i+2:end))]),'econ');
-        U{counter}=Ut;
-        S{counter}=diag(St);
-        V{counter}=Vt;
+        %V{1}的列数：prod(n(2:end))；假设为8；r(1)=3；由于奇异值分解的特点，因此只需要对V的前m=n(1)=3=r(1)个列向量进行后续计算即可；col
+        %最大等于r(i)
+        U{counter}=Ut;%Ut的维度=[n(i+1)*n(i+1)]
+        S{counter}=diag(St);%St的维度=[n(i+1),prod(n(i+2):end)]；diag(St)=n(i+1)个元素
+        V{counter}=Vt;%Vt的维度=[prod(n(i+2):end)*prod(n(i+2):end)]
         Slevel=[Slevel;S{counter}];
         counter=counter+1;
-        if rem(j,length(S{whichvcounter}))==0
+        if rem(j,length(S{whichvcounter}))==0%length(S{1})=n(1)
             whichvcounter =  whichvcounter+1;
         end
     end
-    Slevel=kron(Slevel, ones(nleaf/length(Slevel),1));
+    Slevel=kron(Slevel, ones(nleaf/length(Slevel),1));%这里的nleaf是最后一层的节点数目；最后的目的是把一切都扩展成叶子节点那么长的数组；
     
     
-    sigmas=sigmas.*Slevel;
+    sigmas=sigmas.*Slevel;%第一次求得的奇异值，与后续进一步展开求得的奇异值进行累计乘积运算
 end
 end
